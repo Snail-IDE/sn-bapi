bad3733d6b53c93135781db6eb4410e4ae751d93
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2022-10-30 19:36:30 -0600

    support for data uri

--- pages/api/getSize.js
+++ pages/api/getSize.js
@@ -15,6 +15,9 @@ export default async (req, res) => {
   if (isValidUrl(url) !== true || url.includes("<" || ">" || "<script>" || "</script>") || encodeURIComponent(url).includes("%3C" || "%3E" || "%20")) {
     return res.status(400).setHeader('Content-Type', 'text/plain').send("lol");
   }
+  if (String(url).startsWith("data:image/")) {
+    url = Buffer.from(String(url).split(",")[1], "base64")
+  }
   Jimp.read(url, (err, image) => {
     if (err) return res.status(400).setHeader('Content-Type', 'text/plain').send("bruh");
     res.setHeader("Content-Type", 'application/json');
--- pages/api/nanoid.js
+++
@@ -1,9 +0,0 @@
-import { nanoid } from 'nanoid'
-
-export default (req, res) => {
-    let length = req.query.length || 10
-    let object = new Object();
-    object['nanoid'] = nanoid(length);
-    res.setHeader("Content-Type", 'application/json');
-    res.send(JSON.stringify(object, null, 4));
-}
