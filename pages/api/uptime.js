ce8e91f34bb2740660cccd5aa98ee0ac15f6bde2
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2024-04-04 21:47:35 -0600

    Revert "Create uptime.js"
    This reverts commit 7d6fa582054536d07e9b48ec2225afa7b86a678f.

--- pages/api/uptime.js
+++
@@ -1,8 +0,0 @@
-// yes this is hosted statically, still wondering though
-const startDate = Date.now();
-
-export default async (req, res) => {
-    res.status(200)
-        .setHeader('Content-Type', 'application/json')
-        .json({ 'time': Date.now() - startDate });
-}
